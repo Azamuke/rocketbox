191e7d98b0a77873590fdb1ae47e3ed00ae2ff55
Azamuke <e.m.macedo2012@gmail.com>
2019-04-22 16:31:34 -0300

    mobile (react-native)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Route from "./routes";
+import Routes from "./routes";
 
 class App extends Component {
   render() {
-    return <Route />;
+    return <Routes />;
   }
 }
 
